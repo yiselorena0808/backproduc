3fe3c47e00af94ced59b9bd466516ece9cc8e67d
miguel <miguelanmartinez717@gmail.com>
2025-08-14 16:58:36 -0500

    save

--- app/middleware/auth_jwt.ts
+++ app/middleware/auth_jwt.ts
@@ -1,19 +1,23 @@
 import Jwt from 'jsonwebtoken';
+import type { HttpContext} from '@adonisjs/core/http'
 
 const SECRET=process.env.jwt_secret || 'sstrict';
 export default class AuthJwt{
-    async handle({request,response}, next){
+    async handle({request,response}: HttpContext, next: any){
         const authheader = request.header('Authorization');
+        if (!authheader) {
+  throw new Error('Authorization header missing')
+}
         const token = authheader.replace('Bearer', '').trim()
         if (!token){
-            return response.unathorized({message:"Falta un token"})
+            return response.unauthorized({message:"Falta un token"})
         } else {
             try {
                 const jwtcoded = Jwt.verify(token, SECRET)
                 request.updateBody({authUsuario: jwtcoded})
                 await next()
             } catch (error) {
-                return response.unathorized({message:"Token invalido"})
+                return response.unauthorized({message:"Token invalido"})
             }
         }
     }
